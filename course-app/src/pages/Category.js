7ebcbb83b89efbf965b9428a058f1566c63b010b
momongako <kuuhaku771998@gmail.com>
2022-07-05 14:24:39 +0700

    update

--- course-app/src/pages/Category.js
+++ course-app/src/pages/Category.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Category = () => {
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    console.log('app useeffect!!');
+    let url = 'https://62c253232af60be89ed60e41.mockapi.io/Courses';
+    // if (search.length > 0) {
+    //     url = url + '?search=' + search;
+    // }
+
+    console.log(url);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data); //setStudents(data)
+      });
+    console.log('>>> check dataUse : ', data);
+
+  }, []);
   return (
     <div className="container-fluid py-5">
+
       <div className="container pt-5 pb-3">
         <div className="text-center mb-5">
           <h5
@@ -14,82 +35,22 @@ const Category = () => {
           <h1>Danh mục phổ biến nhất</h1>
         </div>
         <div className="row">
-          <div className="col-lg-3 col-md-6 mb-4">
-            <div className="cat-item position-relative overflow-hidden rounded mb-2">
-              IMG
-              <a className="cat-overlay text-white text-decoration-none" href>
-                <h4 className="text-white font-weight-medium">Web Design</h4>
-                <span>100 Courses</span>
-              </a>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 mb-4">
-            <div className="cat-item position-relative overflow-hidden rounded mb-2">
-              IMG
-              <a className="cat-overlay text-white text-decoration-none" href>
-                <h4 className="text-white font-weight-medium">Development</h4>
-                <span>100 Courses</span>
-              </a>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 mb-4">
-            <div className="cat-item position-relative overflow-hidden rounded mb-2">
-              IMG
-              <a className="cat-overlay text-white text-decoration-none" href>
-                <h4 className="text-white font-weight-medium">Game Design</h4>
-                <span>100 Courses</span>
-              </a>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 mb-4">
-            <div className="cat-item position-relative overflow-hidden rounded mb-2">
-              IMG
-              <a className="cat-overlay text-white text-decoration-none" href>
-                <h4 className="text-white font-weight-medium">Apps Design</h4>
-                <span>100 Courses</span>
-              </a>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 mb-4">
-            <div className="cat-item position-relative overflow-hidden rounded mb-2">
-              IMG
-              <a className="cat-overlay text-white text-decoration-none" href>
-                <h4 className="text-white font-weight-medium">Marketing</h4>
-                <span>100 Courses</span>
-              </a>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 mb-4">
-            <div className="cat-item position-relative overflow-hidden rounded mb-2">
-              IMG
-              <a className="cat-overlay text-white text-decoration-none" href>
-                <h4 className="text-white font-weight-medium">Research</h4>
-                <span>100 Courses</span>
-              </a>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 mb-4">
-            <div className="cat-item position-relative overflow-hidden rounded mb-2">
-              IMG
-              <a className="cat-overlay text-white text-decoration-none" href>
-                <h4 className="text-white font-weight-medium">
-                  Content Writing
-                </h4>
-                <span>100 Courses</span>
-              </a>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 mb-4">
-            <div className="cat-item position-relative overflow-hidden rounded mb-2">
-              IMG
-              <a className="cat-overlay text-white text-decoration-none" href>
-                <h4 className="text-white font-weight-medium">SEO</h4>
-                <span>100 Courses</span>
-              </a>
-            </div>
-          </div>
+          {data.map((item, index) => {
+            return (
+              <div className="col-lg-3 col-md-6 mb-4">
+                <div className="cat-item position-relative overflow-hidden rounded mb-2">
+                  <img class="img-fluid" src={require(`./img/cat-${item.id}.jpg`)} />
+                  <a className="cat-overlay text-white text-decoration-none" href>
+                    <h4 className="text-white font-weight-medium">Web Design</h4>
+                    <span>100 Courses</span>
+                  </a>
+                </div>
+              </div>
+            )
+          })}
         </div>
       </div>
+
     </div>
   );
 };
