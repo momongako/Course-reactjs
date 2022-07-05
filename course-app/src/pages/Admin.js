be981d998d8ffe91626521e0232d9e21502b3ea8
momongako <kuuhaku771998@gmail.com>
2022-07-05 11:40:58 +0700

    clearfix admin

--- course-app/src/pages/Admin.js
+++ course-app/src/pages/Admin.js
@@ -282,28 +282,6 @@ const Admin = () => {
 
         {/* table Admin */}
         {/* Pagination */}
-        <div className="w3-center w3-padding-32">
-          <div className="w3-bar">
-            <a href="#" className="w3-bar-item w3-button w3-hover-black">
-              «
-            </a>
-            <a href="#" className="w3-bar-item w3-black w3-button">
-              1
-            </a>
-            <a href="#" className="w3-bar-item w3-button w3-hover-black">
-              2
-            </a>
-            <a href="#" className="w3-bar-item w3-button w3-hover-black">
-              3
-            </a>
-            <a href="#" className="w3-bar-item w3-button w3-hover-black">
-              4
-            </a>
-            <a href="#" className="w3-bar-item w3-button w3-hover-black">
-              »
-            </a>
-          </div>
-        </div>
 
       </div>
     </div>
