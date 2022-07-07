7f061446fcf81d4d3fe318f070133a2c66f6d80e
nqm0711 <qm0711@gmail.com>
2022-07-07 12:35:05 +0700

    12121

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -16,8 +16,10 @@ const Admin = () => {
   const [currentItems, setCurrentItems] = useState(null);
   const [pageCount, setPageCount] = useState(0);
   const [page, setPage] = useState(-1);
-  const [show, setShow] = useState(false);
-
+  const [showEdit, setEditShow] = useState(false);
+  const [showNew, setNewShow] = useState(false);
+  const [direction, setDirection] = useState(1)
+  const [entries, setEntries] = useState(5)
   useEffect(() => {
     setProducts(data);
     if (products != null) {
@@ -26,20 +28,26 @@ const Admin = () => {
   }, [data]);
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleEditShow = () => setEditShow(true);
+  const handleNewShow = () => setNewShow(true);
+  const handleCancle=()=>{setShow(false);}
 
-  useEffect(() => {
-    if (products !== null) {
-      let itemsPerPage = 5;
+
+  const pageHandler =(data)=>{
+    if (data !== null) {
+      let itemsPerPage = entries;
       const starOffset = page * itemsPerPage;
       let endOffset = (page + 1) * itemsPerPage;
-      if (endOffset > products.length) {
-        endOffset = products.length;
+      if (endOffset > data.length) {
+        endOffset = data.length;
       }
-      setCurrentItems(products.slice(starOffset, endOffset));
-      setPageCount(Math.ceil(products.length / itemsPerPage));
+      setCurrentItems(data.slice(starOffset, endOffset));
+      setPageCount(Math.ceil(data.length / itemsPerPage));
     }
-  }, [page]);
+  }
+  useEffect(() => {
+    pageHandler(products)
+  }, [page]||[entries]);
 
   const handlePageClick = (event) => {
     setPage(event.selected);
@@ -49,176 +57,97 @@ const Admin = () => {
   if (products !== null && currentItems !== null) {
     products_list = currentItems.map((item, key) => (
       <tr key={key}>
-        <td>{item.id}</td>
-        <td>{item.name}</td>
-        <td>{item.category}</td>
-        <td>{item.level>75?'Expert':item.level>50?'Advanced':item.level>25?'Intermediate':'Beginner'}</td>
-        <td><img src={item.picture} alt='' width='50px' height='50px'/></td>
-        <td>${item.price}</td>
-        <td>
-          <button onClick={handleShow} className="btn btn-primary mx-1 adminEditButton" data-toggle="modal">
-          <i class="fa-solid fa-pen-to-square"></i>
+        <td className='adminTableSTT'>{item.id}</td>
+        <td className='adminTableCName'>{item.name}</td>
+        <td className='adminTableCat'>{item.category}</td>
+        <td className='adminTableLevel'>{item.level>75?'Expert':item.level>50?'Advanced':item.level>25?'Intermediate':'Beginner'}</td>
+        <td className='adminTablePic'><img src={item.picture} alt='' width='50px' height='50px'/></td>
+        <td className='adminTablePrice'>${item.price}</td>
+        <td className='adminTableEdit'>
+          <button onClick={handleEditShow} className="btn btn-primary mx-1 adminEditButton" data-toggle="modal">
+          <i className="fa-solid fa-pen-to-square"></i>
           </button>
           <button className="btn btn-danger mx-1 adminDelete,Button" data-toggle="modal">
-          <i class="fa-solid fa-trash"></i>
+          <i className="fa-solid fa-trash"></i>
           </button>
         </td>
       </tr>
     ));
   }
+  
+  const SortColumn = (event,field, type) => {
+    const sortData = [...products];
+    // const sortData = [...currentItems];
+    if (type === 'number') {
+      sortData.sort((a, b) => direction * (a[field] - b[field]));
+    } else if (type === 'string') {
+      sortData.sort(
+        (a, b) =>
+        direction*(a[field].toLowerCase().localeCompare(b[field].toLowerCase()))
+      );
+    }
+    var otherClass = document.querySelector('.theadRow th')
+    otherClass.classList.value = "sort"
+    
+    var itemClass = (event.target.classList);
+    itemClass.value=('sort'+" "+(direction===1?'asc':'desc'));
+    setDirection(direction * -1);
+    pageHandler(sortData)
+  };
 
-  return products !== null ? (
+  return currentItems !== null ? (
     <div>
       <>
-        <Modal show={show} onHide={handleClose}>
+        <Modal show={showEdit} onHide={handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Edit Course</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <AdminModal />
           </Modal.Body>
         </Modal>
       </>
-      <nav
-        className=" w3-collapse w3-animate-left"
-        style={{ zIndex: 3, width: "300px", height: "46%" }}
-        id="mySidebar"
-      >
-        <br />
-        <div className="w3-container">
-          <a
-            href="#"
-            onClick="w3_close()"
-            className="w3-hide-large w3-right w3-jumbo w3-padding w3-hover-grey"
-            title="close menu"
-          >
-            <i className="fa fa-remove" />
-          </a>
-          <img
-            src="https://www.w3schools.com/w3images/avatar_g2.jpg"
-            style={{ width: "45%" }}
-            className="w3-round"
-          />
-          <br />
-          <br />
-          <h4>
-            <b>PORTFOLIO</b>
-          </h4>
-          <p className="w3-text-grey">Template by W3.CSS</p>
-        </div>
-        <div className="w3-bar-block">
-          <a
-            onClick="w3_close()"
-            className="w3-bar-item w3-button w3-padding w3-text-teal"
-          >
-            <i className="fa fa-th-large fa-fw w3-margin-right" />
-            PORTFOLIO
-          </a>
-          <a
-            href="#about"
-            onClick="w3_close()"
-            className="w3-bar-item w3-button w3-padding"
-          >
-            <i className="fa fa-user fa-fw w3-margin-right" />
-            ABOUT
-          </a>
-          <a
-            href="#contact"
-            onClick="w3_close()"
-            className="w3-bar-item w3-button w3-padding"
-          >
-            <i className="fa fa-envelope fa-fw w3-margin-right" />
-            CONTACT
-          </a>
-        </div>
-        <div className="w3-panel w3-large">
-          <i className="fa fa-facebook-official w3-hover-opacity" />
-          <i className="fa fa-instagram w3-hover-opacity" />
-          <i className="fa fa-snapchat w3-hover-opacity" />
-          <i className="fa fa-pinterest-p w3-hover-opacity" />
-          <i className="fa fa-twitter w3-hover-opacity" />
-          <i className="fa fa-linkedin w3-hover-opacity" />
-        </div>
-      </nav>
-
-      <div
-        className="w3-overlay w3-hide-large w3-animate-opacity"
-        onClick="w3_close()"
-        style={{ cursor: "pointer" }}
-        title="close side menu"
-        id="myOverlay"
-      />
-
-      <div className="w3-main" style={{ marginLeft: "300px" }}>
-        <header id="portfolio">
-          <a href="#">
-            <img
-              src="img/blog-1.jpg"
-              style={{ width: "65px" }}
-              className="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"
-            />
-          </a>
-          <span
-            className="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey"
-            onClick="w3_open()"
-          >
-            <i className="fa fa-bars" />
-          </span>
-          <div className="w3-container">
-            <h1>
-              <b>Trang quản lý</b>
-            </h1>
-            <div className="w3-section w3-bottombar w3-padding-16">
-              <span className="w3-margin-right">Filter:</span>
-              <button className="w3-button w3-black">ALL</button>
-              <button className="w3-button w3-white">
-                <i className="fa fa-diamond w3-margin-right" />
-                Design
-              </button>
-              <button className="w3-button w3-white w3-hide-small">
-                <i className="fa fa-photo w3-margin-right" />
-                Photos
-              </button>
-              <button className="w3-button w3-white w3-hide-small">
-                <i className="fa fa-map-pin w3-margin-right" />
-                Art
-              </button>
-            </div>
-          </div>
-        </header>
-
+      <>
+        <Modal show={showNew} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Create New Course</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <AdminModal />
+          </Modal.Body>
+        </Modal>
+      </>
+      <div className="container p-0">
         <div className="container">
           <div className="table-responsive">
-            <div className="table-wrapper">
+            <div className="table-wrapper col-12">
               <div className="table-title">
                 <div className="row">
-                  <div className="col-xs-6">
-                    <h2>
-                      Manage <b>Employees</b>
+                  <div className="col-6">
+                    <h2 className="text-white fs-3">
+                      Manage <b className="text-success fs-2 fw-bolder fw-italic">Employees</b>
                     </h2>
                   </div>
-                  <div className="col-xs-6">
-                    <a
-                      onClick={handleShow}
+                  <div className="col-6 my-auto">
+                    <button
+                      onClick={handleNewShow}
                       className="btn btn-success"
                       data-toggle="modal"
                     >
-                      <i className="material-icons"></i>{" "}
-                      <span>Add New Employee</span>
-                    </a>
+                      <i className="fa-solid fa-circle-plus"></i>
+                      <span className="py-1 px-1">Add New Employee</span>
+                    </button>
                   </div>
                 </div>
               </div>
-              <div className="card-body">
+              <div className="card-body col-12">
                 <div className="dataTable-wrapper dataTable-loading no-footer sortable searchable fixed-columns">
                   <div className="dataTable-top">
                     <div className="dataTable-dropdown">
                       <label>
-                        <select className="dataTable-selector">
+                        <select className="dataTable-selector" onChange={(e)=>{setEntries(e.target.value)}}>
                           <option value={5}>5</option>
-                          <option value={10} selected>
-                            10
-                          </option>
+                          <option value={10}>10</option>
                           <option value={15}>15</option>
                           <option value={20}>20</option>
                           <option value={25}>25</option>
@@ -237,27 +166,26 @@ const Admin = () => {
                   <div className="dataTable-container">
                     <table id="datatablesSimple" className="dataTable-table">
                       <thead>
-                        <tr>
-                          <th>
-                              No.
+                        <tr className="theadRow">
+                          <th className="sort" data-sort="number" onClick={(e)=>SortColumn(e,'id','number')}>
+                            No.
                           </th>
-                          <th>
+                          <th className="sort" data-sort="name" onClick={(e)=>SortColumn(e,'name','string')}>
                               Course Name
                           </th>
-                          <th>
+                          <th className="sort" data-sort="category" onClick={(e)=>SortColumn(e,'category','string')}>
                               Category
                           </th>
-                          <th>
+                          <th className="sort" data-sort="level" onClick={(e)=>SortColumn(e,'level','number')}>
                               Level
                           </th>
                           <th>
                               Picture
                           </th>
-                          <th>
+                          <th className="sort" data-sort="price" onClick={(e)=>SortColumn(e,'price','number')}>
                               Price
                           </th>
                           <th>
-                              Salary
                           </th>
                         </tr>
                       </thead>
@@ -268,7 +196,7 @@ const Admin = () => {
                   </div>
                   <div className="clearfix">
                 <div className="hint-text">
-                  Showing <b>5</b> out of <b>{products.length}</b> entries
+                  Showing <b>{entries}</b> out of <b>{products.length}</b> entries
                 </div>
                 <ReactPaginate
                   previousLabel="Previous"
@@ -299,10 +227,10 @@ const Admin = () => {
       </div>
     </div>
   ) : (
-    <div class="text-center">
-      <button class="btn btn-primary" type="button" disabled>
+    <div className="text-center">
+      <button className="btn btn-primary" type="button" disabled>
         <span
-          class="spinner-grow spinner-grow-sm"
+          className="spinner-grow spinner-grow-sm"
           role="status"
           aria-hidden="true"
         ></span>
