17e4e7034142270878dd1c23436302d4f1f43144
momongako <kuuhaku771998@gmail.com>
2022-07-07 10:02:32 +0700

    loading category

--- src/pages/CourseCategory.js
+++ src/pages/CourseCategory.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
+import Loading from './Loading';
 const CourseCategory = () => {
     const [data, setData] = useState([]);
     const [category_namme, setName] = useState(null);
@@ -31,32 +32,36 @@ const CourseCategory = () => {
                         <h1> Khóa học {category_namme} </h1>
                     </div>
                     <div className="row">
-                        {data.map((item, index) => {
-                            return (
-                                <div className="col-lg-4 col-md-6 mb-4">
-                                    <div className="rounded overflow-hidden mb-2">
-                                        <img className="img-fluid" src={require(`./img/course-${1}.jpg`)} />
-                                        <div className="bg-secondary p-4">
-                                            <div className="d-flex justify-content-between mb-3">
-                                                <small className="m-0"><i className="fa fa-users text-primary mr-2" />{item.rating} Students</small>
-                                                <small className="m-0"><i className="far fa-clock text-primary mr-2" />
-                                                    {moment(item.date.slice(0, 10), 'DD-MM-YYYY').format('DD/MM/YYYY')}
-                                                </small>
-                                            </div>
-                                            <Link className="h5" to={'/detail/' + item.id}>{item.category} &amp; {item.name}</Link>
-                                            <div className="border-top mt-4 pt-4">
-                                                <div className="d-flex justify-content-between">
-                                                    <h6 className="m-0"><i className="fa fa-star text-primary mr-2" />4.5
-                                                        <small>{item.level}</small>
-                                                    </h6>
-                                                    <h5 className="m-0">${item.price} </h5>
+                        {data && data.length > 0 ?
+                            data.map((item, index) => {
+                                return (
+                                    <div className="col-lg-4 col-md-6 mb-4">
+                                        <div className="rounded overflow-hidden mb-2">
+                                            <img className="img-fluid" src={require(`./img/course-${1}.jpg`)} />
+                                            <div className="bg-secondary p-4">
+                                                <div className="d-flex justify-content-between mb-3">
+                                                    <small className="m-0"><i className="fa fa-users text-primary mr-2" />{item.rating} Students</small>
+                                                    <small className="m-0"><i className="far fa-clock text-primary mr-2" />
+                                                        {moment(item.date.slice(0, 10), 'DD-MM-YYYY').format('DD/MM/YYYY')}
+                                                    </small>
+                                                </div>
+                                                <Link className="h5" to={'/detail/' + item.id}>{item.category} &amp; {item.name}</Link>
+                                                <div className="border-top mt-4 pt-4">
+                                                    <div className="d-flex justify-content-between">
+                                                        <h6 className="m-0"><i className="fa fa-star text-primary mr-2" />4.5
+                                                            <small>{item.level}</small>
+                                                        </h6>
+                                                        <h5 className="m-0">${item.price} </h5>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        })}
+                                )
+                            })
+                            :
+                            <Loading />
+                        }
 
                     </div>
                 </div>
