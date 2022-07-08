dc36d401bc5abae8f0662c6313445e1cf2da0012
nqm0711 <qm0711@gmail.com>
2022-07-08 12:41:57 +0700

    AAAAAA

--- src/pages/CourseEdit.js
+++ src/pages/CourseEdit.js
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { fetchCourseActions, fetchCategoryActions } from "../store/Fetch";
 import { useSelector, useDispatch } from "react-redux";
+import Scroll from "../components/Scroll";
 
 const CourseEdit = () => {
   const params = useParams();
@@ -61,7 +62,6 @@ const CourseEdit = () => {
     }
   }, [data]);
 
-  console.log(categories);
 
   const saveProduct = () => {
     let method = "POST";
@@ -82,22 +82,39 @@ const CourseEdit = () => {
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         navigate(-1);
       });
   };
 
   const handleChange = (event) => {
-    console.log(event);
     const target = event.target;
     const value = target.value;
     const name = target.name;
 
     let data = { ...product };
     data[name] = value;
-    console.log(name, value);
     setProduct(data);
   };
+
+  const handleSectionName=(event,sectionNumber)=>{
+    const target = event.target;
+    const value = target.value;
+
+    let data = { ...product };
+    let content = data.content;
+    Object.values(content[sectionNumber])[1] = value
+    setProduct(data)
+  }
+
+  const handleSectionContent=(event,parent)=>{
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    let data = {...product};
+    let content = data.content;
+    Object.values(content[parent])[0][name]=value
+    setProduct(data)
+  }
 
   return (
     <>
@@ -107,166 +124,177 @@ const CourseEdit = () => {
             <div className="table-responsive">
               <div className="table-wrapper col-12">
                 <div className="table-title">
-                    <div className="col-12">
-                      <h2 className="text-white fs-3">
-                        Manage{" "}
-                        <b className="text-special fs-2 fw-bolder fw-italic">
-                          Courses
-                        </b>
-                      </h2>
-                    </div>
-                    </div>
-                    <div className="container">
-                    <div className="container card-body">
-                      <div className="container dataTable-wrapper">
-                        <div className="col-lg-10 col-md-12 mx-auto col-sm-12">
-                          <h1><strong>
+                  <div className="col-12">
+                    <h2 className="text-white fs-3">
+                      Manage{" "}
+                      <b className="text-special fs-2 fw-bolder fw-italic">
+                        Courses
+                      </b>
+                    </h2>
+                  </div>
+                </div>
+                <div className="container">
+                  <div className="container card-body">
+                    <div className="container dataTable-wrapper">
+                      <div className="col-lg-10 col-md-12 mx-auto col-sm-12">
+                        <h1>
+                          <strong>
                             {product.id ? "EDIT COURSE" : "ADD NEW COURSE"}
-                          </strong></h1>
-                          <div className="table-responsive">
-                            <table className="table table-user-information">
-                              <tbody>
-                                <tr>
-                                  {product.id ? (
-                                    <td>
-                                      <strong>Course ID</strong>
-                                    </td>
-                                  ) : null}
-                                  <td className="text-primary">{product.id}</td>
-                                </tr>
-                                <tr>
+                          </strong>
+                        </h1>
+                        <div className="table-responsive">
+                          <table className="table table-user-information">
+                            <tbody>
+                              <tr>
+                                {product.id ? (
                                   <td>
-                                    <strong>Course's Name</strong>
+                                    <strong>Course ID</strong>
                                   </td>
-                                  <td>
-                                    <input
-                                      type="text"
-                                      className="form-control"
-                                      value={product.name}
-                                      name="name"
-                                      onChange={(e) => handleChange(e)}
-                                    ></input>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td>
-                                    <strong>Category</strong>
-                                  </td>
-                                  <td>
-                                    <select
-                                      name="category"
-                                      value={product.category}
-                                      onChange={(e) => {
-                                        handleChange(e);
-                                      }}
-                                    >
-                                      {categories != null ? (
-                                        categories.map((item) => (
-                                          <option
-                                            key={item.id}
-                                            value={item.name}
-                                          >
-                                            {item.name}
-                                          </option>
-                                        ))
-                                      ) : (
-                                        <option key={item.id} value="loading">
-                                          loading
+                                ) : null}
+                                <td className="text-primary">{product.id}</td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <strong>Course's Name</strong>
+                                </td>
+                                <td>
+                                  <input
+                                    type="text"
+                                    className="form-control"
+                                    value={product.name}
+                                    name="name"
+                                    onChange={(e) => handleChange(e)}
+                                  ></input>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <strong>Category</strong>
+                                </td>
+                                <td>
+                                  <select
+                                    name="category"
+                                    value={product.category}
+                                    onChange={(e) => {
+                                      handleChange(e);
+                                    }}
+                                  >
+                                    {categories != null ? (
+                                      categories.map((item) => (
+                                        <option key={item.id} value={item.name}>
+                                          {item.name}
                                         </option>
-                                      )}
-                                    </select>
-                                  </td>
-                                </tr>
+                                      ))
+                                    ) : (
+                                      <option key={item.id} value="loading">
+                                        loading
+                                      </option>
+                                    )}
+                                  </select>
+                                </td>
+                              </tr>
 
-                                <tr>
-                                  <td>
-                                    <strong>Price</strong>
-                                  </td>
-                                  <td>
-                                    <input
-                                      type="number"
-                                      className="form-control"
-                                      value={product.price}
-                                      name="price"
-                                      onChange={(e) => handleChange(e)}
-                                    ></input>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td>
-                                    <strong>Picture</strong>
-                                  </td>
-                                  <td class="text-primary">
-                                    <input type='file'/>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td>
-                                    <strong>Description</strong>
-                                  </td>
-                                  <td>
-                                    <textarea
-                                      type="text"
-                                      name="description"
-                                      className="form-control"
-                                      value={product.description}
-                                      onChange={(e) => handleChange(e)}
-                                    ></textarea>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td>
-                                    <strong>Manufaturing Date</strong>
-                                  </td>
-                                  <td>
-                                    <input
-                                      type="date"
-                                      className="form-control w-50"
-                                      value={product.date}
-                                      name="date"
-                                      onChange={(e) => handleChange(e)}
-                                    ></input>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td>
-                                    <strong>Content</strong>
-                                  </td>
-                                  <td>
-                                    {/* <input
-                                      type="date"
-                                      className="form-control w-50"
-                                      value={product.date}
-                                      name="date"
-                                      onChange={(e) => handleChange(e)}
-                                    ></input> */}
-                                    <table className="col-12">
-                                        <thead>
-                                            <th>Section</th>
-                                            <th>Section content</th>
-                                        </thead>
-                                        <tbody>
-                                        </tbody>
-                                    </table>
-                                  </td>
-                                </tr>
-                              </tbody>
-                            </table>
-                            <div>
-                              <button
-                                type="button"
-                                class="btn btn-primary"
-                                onClick={() => saveProduct()}
-                              >
-                                Save
+                              <tr>
+                                <td>
+                                  <strong>Price</strong>
+                                </td>
+                                <td>
+                                  <input
+                                    type="number"
+                                    className="form-control"
+                                    value={product.price}
+                                    name="price"
+                                    onChange={(e) => handleChange(e)}
+                                  ></input>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <strong>Picture</strong>
+                                </td>
+                                <td class="text-primary">
+                                  <input type="file" />
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <strong>Description</strong>
+                                </td>
+                                <td>
+                                  <textarea
+                                    type="text"
+                                    name="description"
+                                    className="form-control"
+                                    value={product.description}
+                                    onChange={(e) => handleChange(e)}
+                                  ></textarea>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <strong>Manufaturing Date</strong>
+                                </td>
+                                <td>
+                                  <input
+                                    type="date"
+                                    className="form-control w-50"
+                                    value={product.date}
+                                    name="date"
+                                    onChange={(e) => handleChange(e)}
+                                  ></input>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <strong>Course Curriculum</strong>
+                                </td>
+                                <td>
+                                    <Scroll>
+                                  <table className="col-12">
+                                    <thead>
+                                     <tr>
+                                        <th>Section</th>
+                                        <th>Section content</th>
+                                      </tr>
+                                    </thead>
+                                    <tbody>
+                                      {product!==null?(product.content.map((item,key1=index) => (
+                                        <>
+                                        {Object.values(item)[0].map((content,key2=index)=>
+                                        <tr key={key2}>
+                                            <td
+                                            rowSpan={
+                                              item.length
+                                            }
+                                            >
+                                             <input value={Object.values(key1)[1]} onChange={(e) => handleSectionName(e,key1)}/>   
+                                            </td>
+                                            <td><input value ={content} name={key2} onChange={(e) => handleSectionContent(e,key1)} /></td>
+                                        </tr>
+                                          )}
+                                        </>
+                                      ))):(<><tr></tr></>)}
+                                    </tbody>
+                                  </table>
+                                  </Scroll>
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                          <div>
+                            <button
+                              type="button"
+                              class="btn btn-primary"
+                              onClick={() => saveProduct()}
+                            >
+                              Save
+                            </button>
+                            <span> </span>
+                            <Link to="/admin">
+                              <button type="button" class="btn btn-secondary">
+                                Cancel
                               </button>
-                              <span> </span>
-                              <Link to="/admin">
-                                <button type="button" class="btn btn-secondary">
-                                  Cancel
-                                </button>
-                              </Link>
-                            </div>
+                            </Link>
                           </div>
                         </div>
                       </div>
@@ -276,6 +304,7 @@ const CourseEdit = () => {
               </div>
             </div>
           </div>
+        </div>
       ) : (
         "loading"
       )}
